118531aa36b23b919d90767a794b6fc738e2703c
jStack●Innovations <wsamson630@gmail.com>
2026-06-15 17:21:49 +0100

    Change navigation path on close event

--- src/Plans/SubscriptionPayment.tsx
+++ src/Plans/SubscriptionPayment.tsx
@@ -106,7 +106,7 @@ export default function SubscriptionPayment() {
       },
 
       onclose: function () {
-        navigate("/plan", { replace: true });
+        navigate("/", { replace: true });
       },
     });
   }
