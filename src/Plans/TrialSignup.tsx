6bb96563445fb2dbeb0f7037ae4df3b0b1cf1354
jStack●Innovations <wsamson630@gmail.com>
2026-06-21 22:39:09 +0100

    Update sign-in link to point to login page

--- src/Plans/TrialSignup.tsx
+++ src/Plans/TrialSignup.tsx
@@ -335,7 +335,7 @@ export default function TrialSignup() {
 
             <p style={{ textAlign: "center", fontSize: 11, color: "#555" }}>
               Already have an account?{" "}
-              <a href="https://dashboard.getenflowai.online" style={{ color: "#d6a86a", textDecoration: "none" }}>Sign in</a>
+              <a href="https://dashboard.getenflowai.online/login" style={{ color: "#d6a86a", textDecoration: "none" }}>Sign in</a>
             </p>
           </div>
 
