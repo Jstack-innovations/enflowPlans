1928f009c527d2eb446cff98c452a041fe1f567d
jStack●Innovations <wsamson630@gmail.com>
2026-07-13 22:20:28 +0100

    Update API_BASE URL for enflowApi

--- src/Config/enflowApi.tsx
+++ src/Config/enflowApi.tsx
@@ -1,4 +1,4 @@
 //export const API_BASE =
 //"https://enflowsubscriptions.onrender.com";
 export const API_BASE =
-"https://enflowsubscriptions-production.up.railway.app";
+"https://api.getenflowai.online/enflowCentralServer";
